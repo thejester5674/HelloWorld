9b51d2d91032a6fb1b02788f9ec4367b36fc32eb
Josh West <thejester5674@gmail.com>
2018-11-09 13:05:35 -0600

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Josh West)
+ * @version (9/7/2018)
+ */
+public class HelloWorld
+{
+    public static void main ()
+    {
+        System.out.println("Hello World!");
+    }
+}
